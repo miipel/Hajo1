1009c584b5286c868a0b27cb87c222360d91e2c1
miipel <mpeltotalo@gmail.com>
2016-11-24 21:50:02 +0200

    Add files via upload

--- SummausPalvelu.java
+++ SummausPalvelu.java
@@ -1,10 +1,12 @@
 package hajo1;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  * 
@@ -13,11 +15,29 @@ import java.net.SocketException;
  */
 public class SummausPalvelu {
 
-	public static void main(String[] args) throws SocketException {
-		DatagramSocket socketUDP = new DatagramSocket();
-		Socket socketTCP = new Socket();
+	private static void main(String[] args) throws Exception {		
+		lahetaUDP();			
+		kuuntele();
+		
 
 	} // main
+
+	private static void lahetaUDP() throws IOException {
+		int porttiNo = 0;
+		String portti = Integer.toString(porttiNo);
+		DatagramSocket socketUDP = new DatagramSocket();
+		byte[] data = portti.getBytes();
+		DatagramPacket paketti = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 3126);		
+		socketUDP.send(paketti);
+		socketUDP.close();
+		
+	}
+
+	private static void kuuntele() throws IOException {
+		// Kuuntele 1-5 s, sen jälkeen lähetä uudelleen
+		// Viidennen uudelleen lähetyksen jälkeen terminate
+		
+	}
 
 	static class SummausPalvelija extends Thread {
 		@Override
